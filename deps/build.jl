ea6e094f41134b1834af8d29ce42fb49ad62d5da
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2018-01-08 13:51:22 +0100

    use flint

--- deps/build.jl
+++ deps/build.jl
@@ -5,10 +5,11 @@ wdir = joinpath(pkgdir, "deps")
 vdir = joinpath(pkgdir, "local")
 sdir = joinpath(pkgdir, "src")
 ldir = joinpath(sdir, "lib")
+flint_dir = joinpath(Pkg.dir("Nemo"), "local", "lib")
 
 cd(ldir)
 
-run(`./configure --prefix=$vdir`)
+run(`./configure --prefix=$vdir --with-flint=$flint_dir`)
 run(`make`)
 run(`make install`)
 run(`make clean`)
--- src/lib/calc_mean.c
+++ src/lib/calc_mean.c
@@ -1,3 +1,16 @@
+#include <flint/ulong_extras.h>
+
+ulong preinv_test(ulong n)
+{
+    return n_preinvert_limb(n);
+}
+
+ulong mult_preinv_test(ulong a, ulong b, ulong n)
+{
+    ulong inv = n_preinvert_limb(n);
+    return n_mulmod2_preinv(a, b, n, inv);
+}
+
 double mean(double a, double b)
 {
     return (a+b) / 2;
