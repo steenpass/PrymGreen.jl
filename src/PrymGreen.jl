56e4f3f357e963a4af9cf6700f7c13e664850cae
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2018-02-05 19:43:02 +0100

    specify package name when calling non-standard function

--- src/PrymGreen.jl
+++ src/PrymGreen.jl
@@ -27,32 +27,33 @@ function load_example(filename::String)
     file = open(filename)
     s = "<root>\n" * readstring(file) * "</root>"
     close(file)
-    example_xml = parse_string(s)
+    example_xml = LightXML.parse_string(s)
     root_xml = LightXML.root(example_xml)
-    key = LightXML.content(find_element(root_xml, "Key"))
-    char = parse(Int64, LightXML.content(find_element(root_xml, "char")))
-    vars = LightXML.content(find_element(root_xml, "vars"))
-    vars = split(vars, ['\n', ' ', '[', ',', ']'], keep=false)
-    vars = Array{String,1}(vars)
-    basis = LightXML.content(find_element(root_xml, "basis"))
-    basis = split(basis, ['\n', ' ', '[', ',', ']'], keep=false)
-    basis = Array{String,1}(basis)
-    free(example_xml)
+    key = LightXML.content(LightXML.find_element(root_xml, "Key"))
+    char = LightXML.content(LightXML.find_element(root_xml, "char"))
+    char = parse(Int64, char)
+    vars = LightXML.content(LightXML.find_element(root_xml, "vars"))
+    vars = split(vars, ['\n', ' ', '[', ',', ']'], keep = false)
+    vars = Array{String, 1}(vars)
+    basis = LightXML.content(LightXML.find_element(root_xml, "basis"))
+    basis = split(basis, ['\n', ' ', '[', ',', ']'], keep = false)
+    basis = Array{String, 1}(basis)
+    LightXML.free(example_xml)
     global X
-    R, X = Singular.PolynomialRing(Fp(char), vars; ordering = :degrevlex,
-            ordering2 = :comp1max)
+    R, X = Singular.PolynomialRing(Singular.Fp(char), vars;
+            ordering = :degrevlex, ordering2 = :comp1max)
     for (i, s) in enumerate(vars)
         eval(parse("$s = X[$i]"))
     end
-    I = Ideal(R, [ eval(parse(s)) for s in basis ])
+    I = Singular.Ideal(R, [ eval(parse(s)) for s in basis ])
     R, I, key
 end
 
-function resort(I::sideal)
+function resort(I::Singular.sideal)
     R = Singular.base_ring(I)
-    J = Ideal(R, [R() for i in 1:ngens(I)]...)
+    J = Singular.Ideal(R, [R() for i in 1:Singular.ngens(I)]...)
     i = 1
-    for j = 1:ngens(I)
+    for j = 1:Singular.ngens(I)
         if Singular.degree(I[j]) != Singular.degree(I[i])
             for k = (j-1):-1:i
                 J[i-1+j-k] = I[k]
@@ -60,8 +61,8 @@ function resort(I::sideal)
             i = j
         end
     end
-    for k = ngens(I):-1:i
-        J[i+ngens(I)-k] = I[k]
+    for k = Singular.ngens(I):-1:i
+        J[i+Singular.ngens(I)-k] = I[k]
     end
     J
 end
@@ -70,31 +71,32 @@ end
 This function can be simplified as soon as maps between polynomial rings are
 available in Singular.jl.
 =#
-function set_degree_bound(R::Singular.PolyRing, I::sideal, d::Int)
-    for i in 1:ngens(I)
+function set_degree_bound(R::Singular.PolyRing, I::Singular.sideal, d::Int)
+    for i in 1:Singular.ngens(I)
         if Singular.degree(I[i]) > d
             error("degree bound too low")
         end
     end
     I.isGB = true
-    r = fres(I, 0, "frame")
-    B = betti(r)
+    r = PrymGreen.fres(I, 0, "frame")
+    B = Singular.betti(r)
     if size(B, 1) > d
         error("degree bound possibly too low to compute free resolution")
     end
-    vars = [ string(Singular.gens(R)[i]) for i in 1:ngens(R) ]
+    vars = [ string(Singular.gens(R)[i]) for i in 1:Singular.ngens(R) ]
     global X
-    S, X = Singular.PolynomialRing(base_ring(R), vars; degree_bound = d,
-            ordering = :degrevlex, ordering2 = :comp1max)
+    S, X = Singular.PolynomialRing(Singular.base_ring(R), vars;
+            degree_bound = d, ordering = :degrevlex, ordering2 = :comp1max)
     for (i, s) in enumerate(vars)
         eval(parse("$s = X[$i]"))
     end
-    Ideal(S, [ eval(parse(string(I[i]))) for i in 1:ngens(I) ])
+    Singular.Ideal(S,
+            [ eval(parse(string(I[i]))) for i in 1:Singular.ngens(I) ])
 end
 
 function submatrix(r::Singular.sresolution, g::Int, char::Entry_t)
     index = div(g, 2)-2
-    B = betti(r)
+    B = Singular.betti(r)
     size = Msize_t(B[3, index])
     if size != Msize_t(B[2, index+1])
         error("matrix not square")
@@ -112,7 +114,7 @@ println(ordstr);
     values_ptr = icxx"""(entry_t **)malloc(sizeof(entry_t *));"""
     n_values = icxx"""
             check_matrix($values_ptr, $r_ptr, $g, $size, $limit, $char, $ring);
-            """
+        """
 println("n_values = ", n_values)
     if n_values == 0
         error("number of values in prym green matrix must be positive")
@@ -129,13 +131,13 @@ println(key)
 println("g = ", g)
     char = parse(PrymGreen.Entry_t, match(r"(?<=@)(.*)(?=g)", key).match)
 println("char = ", char)
-    I = std(I; complete_reduction = true)
+    I = Singular.std(I; complete_reduction = true)
     I = resort(I)
     I = set_degree_bound(R, I, 3)
     I.isGB = true
     gc()
-    @time r = fres(I, div(g, 2)-2, "single module", use_cache = false,
-            use_tensor_trick = true)
+    @time r = PrymGreen.fres(I, div(g, 2)-2, "single module";
+            use_cache = false, use_tensor_trick = true)
 println(r)
     @time A = submatrix(r, g, char)
 println(map(x -> Int(x), A[1:10]))
--- src/singular_tools.jl
+++ src/singular_tools.jl
@@ -1,14 +1,13 @@
 using Cxx
 using Nemo
 using Singular
-using Singular.libSingular
 
 #=
 modified copy from Singular.jl (commit b277b7c) to make use_cache and
 use_tensor_trick available
 =#
-function id_fres(I::libSingular.ideal, n::Cint, method::String,
-        R::libSingular.ring, use_cache::Bool, use_tensor_trick::Bool)
+function id_fres(I::Singular.libSingular.ideal, n::Cint, method::String,
+        R::Singular.libSingular.ring, use_cache::Bool, use_tensor_trick::Bool)
     s = icxx"""const ring origin = currRing;
             rChangeCurrRing($R);
             syStrategy s = syFrank($I, $n, $method, $use_cache,
@@ -25,14 +24,14 @@ end
 modified copy from Singular.jl (commit b277b7c) to make use_cache and
 use_tensor_trick available
 =#
-function fres{T <: Nemo.RingElem}(id::sideal{T}, max_length::Int,
+function fres{T <: Nemo.RingElem}(id::Singular.sideal{T}, max_length::Int,
         method::String = "complete";
         use_cache::Bool = true, use_tensor_trick::Bool = false)
     id.isGB == false && error("ideal is not a standard basis")
     max_length < 0 && error("length for fres must not be negative")
-    R = base_ring(id)
+    R = Singular.base_ring(id)
     if max_length == 0
-        max_length = ngens(R)+1
+        max_length = Singular.ngens(R)+1
         # TODO: consider qrings
     end
     if (method != "complete"
@@ -41,12 +40,12 @@ function fres{T <: Nemo.RingElem}(id::sideal{T}, max_length::Int,
             && method != "single module")
         error("wrong optional argument for fres")
     end
-    r, length = id_fres(id.ptr, Cint(max_length), method, R.ptr, use_cache,
-            use_tensor_trick)
+    r, length = PrymGreen.id_fres(id.ptr, Cint(max_length), method, R.ptr,
+            use_cache, use_tensor_trick)
     return Singular.sresolution{T}(R, length, r)
 end
 
-function rOrdStr(r::libSingular.ring)
+function rOrdStr(r::Singular.libSingular.ring)
     ordstr = icxx"""rOrdStr($r);"""
     res = unsafe_string(ordstr)
     icxx"""omFree($ordstr);"""
