9a6f6e89995da88b6a93e1f6678603d71a69ae41
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2018-02-06 11:39:41 +0100

    update build process

--- deps/build.jl
+++ deps/build.jl
@@ -1,17 +1,14 @@
-oldwdir = pwd()
-
-pkgdir = dirname(dirname(@__FILE__))
-wdir = joinpath(pkgdir, "deps")
-vdir = joinpath(pkgdir, "local")
-sdir = joinpath(pkgdir, "src")
-ldir = joinpath(sdir, "lib")
+pkg_dir = Pkg.dir("PrymGreen")
+lib_dir = joinpath(pkg_dir, "src", "lib")
+local_dir = joinpath(pkg_dir, "local")
 flint_dir = joinpath(Pkg.dir("Nemo"), "local")
 singular_dir = joinpath(Pkg.dir("Singular"), "local")
 
-cd(ldir)
+oldwdir = pwd()
+cd(lib_dir)
 
-run(`./configure --prefix=$vdir --with-flint=$flint_dir \
-                 --with-singular=$singular_dir`)
+run(`./configure --enable-silent-rules --prefix=$local_dir \
+    --with-flint=$flint_dir --with-singular=$singular_dir`)
 run(`make`)
 run(`make install`)
 run(`make clean`)
