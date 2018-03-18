4d87ad4a8ad4bf35415ef686847c10ad600edf77
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2018-03-18 12:34:50 +0100

    increase entry_t to uint32_t

--- src/lib/prym_green_types.h
+++ src/lib/prym_green_types.h
@@ -10,8 +10,8 @@
 
 #define msize_t uint32_t   // for size of matrix and for module components
 #define nvals_t uint32_t   // for number of values
-#define entry_t uint16_t   // for entries and characteristic
-#define null_entry ((entry_t)65535)   // represents not yet assigned entry
+#define entry_t uint32_t   // for entries and characteristic
+#define null_entry ((entry_t)4294967295)   // represents not yet assigned entry
 // uint64_t is ulong
 #define arith_t uint64_t   // for modular arithmetic
 
