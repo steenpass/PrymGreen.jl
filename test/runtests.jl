2697fc96048b51999241a6b1a3edfad2756d85ea
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2019-01-11 12:45:29 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using PrymGreen
+using Random
 using Test
 
-# write your own tests here
-@test 1 == 2
+@test run_example("./data/PCNC@26431g12_2%1.sd"; print_info = false)
+@test string(PrymGreen.random_PCNC(8, 2, Random.MersenneTwister(0))) * "\n" ==
+        read(open("./data/random_PCNC", "r"), String)
