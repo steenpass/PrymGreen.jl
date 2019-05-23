682fa781e2ca931e44bb38d0da6bdd95729791bf
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2019-05-23 12:23:39 +0200

    add multiplication_data()

--- src/multiplication.jl
+++ src/multiplication.jl
@@ -22,3 +22,54 @@ function needed_blocks(g::Int)
     return vcat(B1, B2, B3)
 end
 
+h_shift(h, v, f) = binomial(h+v+f-3, h-1)
+v_shift(h, v, f) = binomial(h+v+f-3, v-1)
+
+function f_multiply(D::Array{Tuple{Int, Int, Int, Int}, 1}, h::Int, v::Int,
+        s::Int, i::Msize_t, j::Msize_t, p::Msize_t)
+    size = h_shift(h, v, 1)   # == v_shift(h, v, 1)
+    for k in 0:(size-1)
+        push!(D, (i+k, j+k, p, s))
+    end
+end
+
+function h_multiply(D::Array{Tuple{Int, Int, Int, Int}, 1}, v::Int, f::Int,
+        s::Int, i::Msize_t, j::Msize_t, p::Msize_t)
+    size = v_shift(1, v, f)
+    for k in 0:(size-1)
+        push!(D, (i+k, j, p+size-k-1, s))
+        s *= -1
+    end
+end
+
+function v_multiply(D::Array{Tuple{Int, Int, Int, Int}, 1}, h::Int, f::Int,
+        s::Int, i::Msize_t, j::Msize_t, p::Msize_t)
+    size = h_shift(h, 1, f)
+    for k in 0:(size-1)
+        push!(D, (i, j+k, p+k, s))
+    end
+end
+
+function multiplication_data_recursion(D::Array{Tuple{Int, Int, Int, Int}, 1},
+        h::Int, v::Int, f::Int, s::Int, i::Msize_t, j::Msize_t, p::Msize_t)
+    (h < 1 || v < 1 || f < 1) && return
+    (f == 1)           && return f_multiply(D, h, v, s, i, j, p)
+    (h == 1 && f == 2) && return h_multiply(D, v, f, s, i, j, p)
+    (v == 1)           && return v_multiply(D, h, f, s, i, j, p)
+    multiplication_data_recursion(D, h-1, v, f, s, i, j, p)
+    j += Msize_t(h_shift(h-1, v, f))
+    f_p = p+Msize_t(binomial(h+v+f-4, f-1))
+    multiplication_data_recursion(D, h, v, f-1, s, i, j, f_p)
+    i += Msize_t(v_shift(h, v, f-1))
+    s *= (f%2)*2-1
+    multiplication_data_recursion(D, h, v-1, f, s, i, j, p)
+end
+
+function multiplication_data(h::Int, v::Int, f::Int)
+    D = Array{Tuple{Int, Int, Int, Int}, 1}(undef, 0)
+    (i, j, p) = (Msize_t(0), Msize_t(0), Msize_t(0))
+    multiplication_data_recursion(D, h, v, f, 1, i, j, p)
+    sort!(D)
+    return D
+end
+
