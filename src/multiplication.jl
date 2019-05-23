8ab2782b91d2035a543246bd565e5f30a752c3a0
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2019-05-23 12:23:12 +0200

    add needed_blocks()

--- src/multiplication.jl
+++ src/multiplication.jl
@@ -11,3 +11,14 @@ function max_modulus(g::Int)
     return ceil(Entry_t, sqrt(2.0^E/N))
 end
 
+function needed_blocks(g::Int)
+    (g % 2 == 0 && g > 6) || error("invalid input")
+    (v, f) = (div(g, 2)-3, 2)
+    B1 = [ (h, v, f) for h in 1:(div(g, 2)-1) ]
+    (v, f) = (div(g, 2)-4, 3)
+    B2 = [ (h, v, f) for h in 1:(div(g, 2)-1) if v > 0 ]
+    (v, f) = (div(g, 2)-5, 4)
+    B3 = [ (h, v, f) for h in 1:(div(g, 2)-2) if v > 0 ]   # !
+    return vcat(B1, B2, B3)
+end
+
